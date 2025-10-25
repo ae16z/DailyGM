e1e730075d324676a7fa6515e5af103d75ec7c3e
ae16z.eth <104980058+ae16z@users.noreply.github.com>
2025-10-25 19:12:07 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+// app/layout.tsx
+import { Providers } from './providers';
+import '@rainbow-me/rainbowkit/styles.css';
+import './globals.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
